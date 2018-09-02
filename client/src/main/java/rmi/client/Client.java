5c895e0274f82e59d9105b802c5fd5257660670c
j1nma <j1nma@users.noreply.github.com>
2018-09-01 21:32:02 -0300

    First commit.

---
+++ client/src/main/java/rmi/client/Client.java
@@ -0,0 +1,12 @@
+package rmi.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("ex2 Client Starting ...");
+    }
+}
